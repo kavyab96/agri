e64e71ca384301e8312f397c05cd2fa132d0d299
kavyab96 <kavyababuji@gmail.com>
2021-04-05 21:04:18 +0530

    first commit

---
+++ link/sample.php
@@ -0,0 +1,10 @@
+<html>
+	<body>
+		
+		<form>
+			<input type="text" name="user" placeholder="User name"></br>
+			<input type="text" name="pass" placeholder="Password"></br>
+			<input type="submit" value="Login">
+		</form>
+	</body>
+</html>
